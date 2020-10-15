374f940234d868bcd8ddff0f90a7c1a4b59796ab
Mark <Marcusa.helms@gmail.com>
2020-10-14 23:02:06 -0500

    less sloppy i hope

--- Practice/BigO.js
+++ Practice/BigO.js
@@ -1,36 +1,41 @@
-// single line loops write BigO 
+/ single line loops write BigO 
 
 //Constant time: O(1)
 //Linear time: O(n)
 //Polynomial time: O(n^k)
 
 const Constant = (a, b) => {
-    console.log(a+ b);
       return a+ b;
   }
   
-  Constant(2,4);
-  
+  let arr2=[1,2,3,4,5,6,7,8,9];
   let arr=[1,2,3,4,5,6,7,8,9];
   
-  const Linear = (array) => {
-     const newArr =[];
-      array.forEach(a => {if(a % 2=== 1){newArr.push(a)}});
-      console.log(newArr);
-      return newArr;
-  }
-  
-  Linear(arr);
-  
-  let arr2=[1,2,3,4,5,6,7,8,9];
-  let arr3=[1,2,3,4,5,6,7,8,9];
-  
-  const Polynomial = (array, array2) => {
-     const newObj=[];
-      array.forEach(a =>{newObj.push(newObj.map(a =>a.push(array2)) )});
-      console.log(newObj);
-      return newObj;
+  const Linear = (array, array2) => {
+     const newArr=[];
+      array.forEach(num =>{
+        newArr.push(array2);
+        });
+      return newArr;  
     }
   
-    Polynomial(arr3,arr2);
+   const DDArr = Linear(arr,arr2);
   
+
+const Polynomial = (arrTwoD) => {
+  let i = 0;
+  let sum = 0;
+  while(i< arrTwoD.length){
+    console.log(1);
+    arrTwoD.forEach(function(number) {
+      console.log("----");
+        if (number % 2 !== 0){
+            sum += number;
+            console.log(2);  
+        }})
+        i++
+        console.log("-------");
+  }
+  console.log(sum);
+}
+Polynomial(DDArr);
